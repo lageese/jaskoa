2405554c92882adc1d2285f52eb224ce41c1c351
deanabia <dea22gonzalez@gmail.com>
2025-11-04 22:40:28 +0100

    Update EncuestaDaoFactory.java

--- ws-app-model/src/main/java/es/udc/ws/app/model/encuesta/EncuestaDaoFactory.java
+++ ws-app-model/src/main/java/es/udc/ws/app/model/encuesta/EncuestaDaoFactory.java
@@ -1,6 +1,7 @@
 package es.udc.ws.app.model.encuesta;
 
 public class EncuestaDaoFactory {
+
     private final static String CLASS_NAME_PARAMETER = "EncuestaDaoFactory.className";
     private static SqlEncuestaDao dao = null;
 
@@ -11,12 +12,15 @@ public class EncuestaDaoFactory {
     private static SqlEncuestaDao getInstance() {
         try {
             String daoClassName = System.getProperty(CLASS_NAME_PARAMETER);
+
             if (daoClassName == null) {
-                // Nombre por defecto de la implementación SQL del DAO
-                daoClassName = "es.udc.ws.app.model.encuesta.SqlEncuestaDao";
+                // Clase por defecto si no se define la propiedad del sistema
+                daoClassName = "es.udc.ws.app.model.encuesta.Jdbc3CcSqlEncuestaDao";
             }
+
             Class<?> daoClass = Class.forName(daoClassName);
             dao = (SqlEncuestaDao) daoClass.getDeclaredConstructor().newInstance();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
